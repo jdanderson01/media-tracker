5dd09d174c78c1c17200d47f58ec443cf50433d5
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-11-26 02:49:22 -0500

    Skeleton
    Added the skeleton of the project including the constructor for the media entries, the form the user inputs the information in and a function to add the media to the myLibrary object

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+class MediaEntry {
+  constructor() {
+    // constructor for the media entry object
+    this.title = title;
+    this.type = type;
+    this.genre = genre;
+    this.dateConsumed = dateConsumed;
+    this.rating = rating;
+
+    function displayInfo() {
+      console.log(
+        `${this.title} - ${this.type} consumed on ${this.dateConsumed}, Rating: ${this.rating}`
+      );
+    }
+  }
+}
+
+function addMediaToLibrary() {
+  // function to add the input from the form to the myLibrary object
+}
